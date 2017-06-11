7b3a9469c6db30af7d69e3f9c362b0c486128b5c
mfutech <mfutech@users.noreply.github.com>
2017-06-11 22:48:42 +0200

    search for cities if cities is unknown and propose laternate names

--- SwissMeteoBot.py
+++ SwissMeteoBot.py
@@ -1,9 +1,13 @@
 #!/bin/env python
 
 import logging
-import yaml
-from urllib.request import urlopen
+import re
 from io import BytesIO
+from urllib.request import urlopen
+import yaml
+import requests
+from lxml import etree
+
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
@@ -13,6 +17,55 @@ with open("config.yml", 'r') as ymlfile:
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+
+def possible_cities(search):
+    """ possible_cities
+
+    will return list of known cities matching search
+
+    Arg: search : string to search for
+    Return: a list of possible cities
+    """
+    url = 'http://www.prevision-meteo.ch/services'
+    values = {
+        'widget-localite': search,
+        'valider': 'Valider'
+    }
+    res = requests.post(url, values)
+    html_parser = etree.HTMLParser()
+    tree = etree.parse(BytesIO(res.content), html_parser)
+    opts = tree.xpath('//select[@name="widget-localite2"]/option')
+    cities_list = {}
+    for opt in opts:
+        name = opt.text
+        match = re.match(r'(\d+)\s+(.*)(,.*)*', opt.values()[0])
+        if match:
+            value = re.sub(r'[()]', '', match[2])
+            value = (re.sub(r'\s+', '-', value)).lower()
+            cities_list[value] = name
+
+    #cities_list = list(map(lambda x: x.values, opts))
+    return cities_list
+
+
+def get_meteo(city, day):
+    """ get_meteo
+
+    Arg: city (string) for which we need a meteo images
+    Result:
+        - False if city does not exists
+        - image (BytesIO) if successfull
+    """
+    url = f"http://www.prevision-meteo.ch/uploads/widget/{city}_{day}.png"
+    res = requests.get(url)
+    if res.status_code == 500:
+        return False
+    else:
+        ## bot.send_photo(chat_id=msg.chat_id, photo=url)
+        img_meteo = BytesIO(urlopen(url).read())
+        img_meteo.name = 'img_meteo.jpeg'
+        img_meteo.seek(0)
+    return(img_meteo)
 
 def start(bot, update):
     """ Start (callback funtion)
@@ -38,15 +91,15 @@ def meteo(bot, update):
     else:
         bot.send_message(chat_id=msg.chat_id, text='merci de donner un nom de ville connu')
         return()
-    if ville in CONF['villes']:
-        url = f"http://www.prevision-meteo.ch/uploads/widget/{ville}_{day}.png"
-        ## bot.send_photo(chat_id=msg.chat_id, photo=url)
-        img_meteo = BytesIO(urlopen(url).read())
-        img_meteo.name = 'img_meteo.jpeg'
-        img_meteo.seek(0)
+    img_meteo = get_meteo(ville, day)
+    if img_meteo:
         bot.send_photo(chat_id=msg.chat_id, photo=img_meteo)
     else:
-        bot.send_message(chat_id=msg.chat_id, text='ville inconnue')
+        alternate_cities = possible_cities(ville)
+        txt = "Désolé, ville inconnue, essayez celle-ci:\n"
+        for city in alternate_cities:
+            txt += "pour " + alternate_cities[city] + " utiliser: " + city + "\n"
+        bot.send_message(chat_id=msg.chat_id, text=txt)
 
 def usage(bot, update):
     """ help (callback function)
